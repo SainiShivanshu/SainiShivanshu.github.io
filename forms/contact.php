3ca02c0095ca2cef8aeacc8d5c995bd0c59f577e
SainiShivanshu <shivanshusaini216@gmail.com>
2022-04-21 15:38:27 +0530

    portpolio submit

---
+++ forms/contact.php
@@ -0,0 +1,84 @@
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  // Replace contact@example.com with your real receiving email address
+  // $receiving_email_address = 'shivanshusaini216@gmail.com';
+
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+  // }
+
+  // $contact = new PHP_Email_Form;
+  // $contact->ajax = true;
+  
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+  // $contact->add_message( $_POST['name'], 'From');
+  // $contact->add_message( $_POST['email'], 'Email');
+  // $contact->add_message( $_POST['message'], 'Message', 10);
+
+  // echo $contact->send();
+
+
+
+ 
+if(isset($_POST['submit'])) {
+ $mailto = "shivanshusaini216@gmail.com";  //My email address
+ //getting customer data
+ $name = $_POST['name']; //getting customer name
+ $fromEmail = $_POST['email']; //getting customer email
+ $phone = $_POST['tel']; //getting customer Phome number
+ $subject = $_POST['subject']; //getting subject line from client
+ $subject2 = "Confirmation: Message was submitted successfully | HMA WebDesign"; // For customer confirmation
+ 
+ //Email body I will receive
+ $message = "Cleint Name: " . $name . "\n"
+ . "Phone Number: " . $phone . "\n\n"
+ . "Client Message: " . "\n" . $_POST['message'];
+ 
+ //Message for client confirmation
+ $message2 = "Dear" . $name . "\n"
+ . "Thank you for contacting us. We will get back to you shortly!" . "\n\n"
+ . "You submitted the following message: " . "\n" . $_POST['message'] . "\n\n"
+ . "Regards," . "\n" . "- HMA WebDesign";
+ 
+ //Email headers
+ $headers = "From: " . $fromEmail; // Client email, I will receive
+ $headers2 = "From: " . $mailto; // This will receive client
+ 
+ //PHP mailer function
+ 
+  $result1 = mail($mailto, $subject, $message, $headers); // This email sent to My address
+  $result2 = mail($fromEmail, $subject2, $message2, $headers2); //This confirmation email to client
+ 
+  //Checking if Mails sent successfully
+ 
+  if ($result1 && $result2) {
+    $success = "Your Message was sent Successfully!";
+  } else {
+    $failed = "Sorry! Message was not sent, Try again Later.";
+  }
+ 
+}
+ 
+?>
